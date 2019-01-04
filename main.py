dd7f907a2b32bbc13b706414b56a253024ba59b0
FixingMind5 <aguilarmanu5@gmail.com>
2019-01-03 22:21:44 -0600

    Agregando funciones en nuestro proyecto

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-clients = 'Pablo, Ricardo,'
+clients = 'Pablo, Ricardo'
 
-if __name__ == '__main__':
-    clients += ' Manuel'
+def _add_coma():
+    global clients
+    clients += ', '
+
+def add_cliente(nombre_Cliente):
+    global clients
+    _add_coma()
+    clients += nombre_Cliente
+
+if __name__ == '__main__': 
+    print(clients)
+    add_cliente('Manolo')
+    print(clients)
+    add_cliente('Antonio')
     print(clients)
