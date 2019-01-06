fbfa45f46d2773e2dd5025ace44f577a14b8e507
FixingMind5 <aguilarmanu5@gmail.com>
2019-01-06 13:09:06 -0600

    recursive binarySearch()

--- binary_search.py
+++ binary_search.py
@@ -1,38 +1,26 @@
 import random
 
-def binarySearch(d, t, s, e):
-    x = False
-    m = 0
+def binarySearch(data, target, low, high):
+    if low > high:
+        print("No, there isn't")
+        return False
 
-    if s > e:
-        x = False
+    mid = (low + high) // 2
 
-    while s <= e:
-        m = (s + e) // 2
-
-        if t == d[m]:
-            x = True
-            return m
-            break
-        elif t < d[m]:
-            e = m - 1
-        else:
-            s = m + 1
-
-    return x
-
+    if target == data[mid]:
+        print("Yeah! there is it!")
+        return True
+    elif target < data[mid]:
+        return binarySearch(data, target, low, mid - 1)
+    else:
+        return binarySearch(data, target, mid + 1, high)
 
 
 if __name__ == '__main__':
-    """data = [random.randint(0, 100) for i in range(10)]"""
-    d = []
+    data = [random.randint(0, 100) for i in range(10)]
 
-    for i in range(10):
-        d.append(random.randint(0, 100))
+    data.sort()
+    print(data)
 
-    d.sort()
-    print(d)
-
-    t = int(input("Number: "))
-    found = binarySearch(d, t, 0, len(d) - 1)
-    print("{} number is at {} position".format(t, found))
+    target = int(input("Number: "))
+    found = binarySearch(data, target, 0, len(data) - 1)
