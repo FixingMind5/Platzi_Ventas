8f09a1f31f2089c607ad858ca156be69e2f91daa
FixingMind5 <aguilarmanu5@gmail.com>
2019-01-06 13:11:38 -0600

    IDK

--- binary_search.py
+++ binary_search.py
@@ -24,3 +24,42 @@ if __name__ == '__main__':
 
     target = int(input("Number: "))
     found = binarySearch(data, target, 0, len(data) - 1)
+
+"""import random
+
+def binarySearch(d, t, s, e):
+    x = False
+    m = 0
+
+    if s > e:
+        x = False
+
+    while s <= e:
+        m = (s + e) // 2
+
+        if t == d[m]:
+            x = True
+            returnm
+            break
+        elif t < d[m]:
+            e = m - 1
+        else:
+            s = m + 1
+
+    return x
+
+
+
+if __name__ == '__main__':
+    data = [random.randint(0, 100) for i in range(10)]
+    d = []
+
+    for i inrange(10):
+        d.append(random.randint(0, 100))
+
+    d.sort()
+    print(d)
+
+    t = int(input("Number: "))
+    found = binarySearch(d, t, 0, len(d) - 1)
+    print("{} number is at {} position".format(t, found))"""
